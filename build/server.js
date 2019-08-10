491d04441726e4661b77243cc745d5f4b5f0660d
Tony Park <c3dream@gmail.com>
2019-08-10 01:17:10 -0400

    added post tour

--- build/server.js
+++ build/server.js
@@ -2,18 +2,28 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const bodyParser = __importStar(require("body-parser"));
 const apiGetTours_1 = require("./api/tours/apiGetTours");
 const apiGetTourDetail_1 = require("./api/tours/apiGetTourDetail");
+const apiCreateTour_1 = require("./api/tours/apiCreateTour");
 const app = express_1.default();
+const jsonParser = bodyParser.json();
+// app.use(bodyParser.urlencoded({ extended: true }));
+// const jsonParser = bodyParser.urlencoded({ extended: true });
 // console.log(JSON.parse(JSON.stringify(DataStore.tours)));
 app.get("/", (req, res, next) => {
     res.send("Tour Booking API");
 });
 app.get("/tours", apiGetTours_1.apiGetTours);
 app.get("/tours:/id", apiGetTourDetail_1.apiGetTourDetail);
-app.post("/tours", (req, res, next) => {
-    res.send("Add a new tour...");
-});
+app.post("/tours", jsonParser, apiCreateTour_1.apiCreateTour);
 app.listen(process.env.PORT || 8091, () => console.log("Server started..."));
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import * as bodyParser from "body-parser";
+
 import { DataStore } from "./data/data";
 import { apiGetTours } from "./api/tours/apiGetTours";
 import { apiGetTourDetail } from "./api/tours/apiGetTourDetail";
+import { apiCreateTour } from "./api/tours/apiCreateTour";
 
 const app = express();
+const jsonParser = bodyParser.json();
+// app.use(bodyParser.urlencoded({ extended: true }));
+// const jsonParser = bodyParser.urlencoded({ extended: true });
 
 // console.log(JSON.parse(JSON.stringify(DataStore.tours)));
 
@@ -15,8 +21,6 @@ app.get("/tours", apiGetTours);
 
 app.get("/tours:/id", apiGetTourDetail);
 
-app.post("/tours", (req, res, next) => {
-  res.send("Add a new tour...");
-});
+app.post("/tours", jsonParser, apiCreateTour);
 
 app.listen(process.env.PORT || 8091, () => console.log("Server started..."));
